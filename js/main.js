81ae7f72150051c7c3e4478095e1f03624364767
Abhishek Ghosh <1abhishekword@gmail.com>
2024-05-19 21:20:31 +0530

    Add Scroll To Top & Nevigation Menu

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ window.addEventListener("scroll", function(){
     const header = document.querySelector("header");
     header.classList.toggle("sticky",window.scrollY > 0);
 });
+
 //Portfolio section - Modal
     const projectModals = document.querySelectorAll(".projects-model");
     const imgCards = document.querySelectorAll(".img-card");
@@ -26,17 +27,45 @@ window.addEventListener("scroll", function(){
         });
     });
 
-//Our clients - Swiper
-
 //Website dark/light theme
 
 //Scroll to top button
+const scrollTopBtn = document.querySelector(".scrollToTop-btn");
+
+window.addEventListener("scroll", function(){
+    scrollTopBtn.classList.toggle("active", window.scrollY > 500);
+});
+
+scrollTopBtn.addEventListener("click", function() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
 
 //Navigation menu items active on page scroll
+window.addEventListener("scroll", () => {
+    const sections = document.querySelectorAll("section");
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current =>{
+        let sectionHeight = current.offsetHeight;
+        let sectionTop = current.offsetTop - 50;
+        let id = current.getAttribute("id");
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector(".nav-items a[href*=" + id + "]").classList.add("active");
+        }else{
+            document.querySelector(".nav-items a[href*=" + id + "]").classList.remove("active");
+        }
+    });
+});
+
 
 //Responsive navigation menu toggle
 
 //Scroll reveal animations
+
 //Common reveal options to create reveal animations
 
 //Target elements, and specify options to create reveal animations
